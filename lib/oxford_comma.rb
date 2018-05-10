49689806bc26d45be2f535dced5f7ec738d6eebe
Devin Pierce <devinmpierce@gmail.com>
2018-05-10 01:46:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if (array.length == 1)
     string << array[0]
   elsif (array.length == 2)
-    string << 
+    string << array[0] + ' and ' + array[1] 
   else
     array.each_with_index do |word, i|
       if (i < array.length - 1)

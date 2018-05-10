fc9dd7c1827cadd1a4974320121dbb21207e7850
Devin Pierce <devinmpierce@gmail.com>
2018-05-10 01:45:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,12 @@ def oxford_comma(array)
   string = ''
   if (array.length == 1)
     string << array[0]
+  elsif (array.length == 2)
+    string << 
   else
     array.each_with_index do |word, i|
       if (i < array.length - 1)
-        array.length > 2 ? string << word + ', ' : string << word
+        string << word + ', '
       else
         string << 'and ' + word
       end

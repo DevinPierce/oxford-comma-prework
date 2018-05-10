8dd8b0cbc08d8c2dd430d51a14778ba3210eb11b
Devin Pierce <devinmpierce@gmail.com>
2018-05-10 01:32:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,10 @@ def oxford_comma(array)
   string = ''
   if (array.length == 1)
     string << array[0]
-  end
-  array.each_with_index do |word, i|
-    string << word + ', '
+  else
+    array.each_with_index do |word, i|
+      string << word + ', '
+    end
   end
   string
 end
